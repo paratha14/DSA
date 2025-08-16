3a6ba0f286562eddbd634ceb473031a6b30e7097
Pratham Mohan <pratham.parker14@gmail.com>
2025-08-16 23:14:08 +0530

    solved on 16/08/25

---
+++ 1095-find_in_mountain_array/Solution.java
@@ -0,0 +1,58 @@
+class Solution{
+    public static void main(String[] args) {
+    Solution x= new Solution();
+    int[] test={0,1,2,4,2,1};
+    int result= x.findInMountainArray(3, test);
+    System.out.println("result is: "+result);
+    }
+    public int findInMountainArray(int target, int[]arr) {
+        int peakindex= findPeak(arr);
+        if(target==arr[peakindex]){
+            return peakindex;
+        }
+        int left_index= findElement(arr,target, 0,peakindex-1);
+        int right_index= findElement(arr, target, peakindex+1, arr.length-1);
+        if(left_index!= -1){
+            return left_index;
+        }
+        else{
+            return right_index;
+        }
+
+    }
+
+    public int findPeak(int[]arr){
+        int start=0;
+        int end=arr.length-1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    public int findElement(int[] arr, int target, int start, int end){
+
+            while(start<=end){
+                int mid= start + (end-start)/2;
+
+                if(target==arr[mid]){
+                    return mid;
+                    }
+
+                else if(target>arr[mid]){
+                    start=mid+1;
+                }
+                else if(target<arr[mid]){
+                    end=mid-1;
+                }
+
+            }
+            return -1;
+        }
+
+}
