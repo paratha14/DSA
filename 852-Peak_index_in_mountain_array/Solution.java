dfdcaa411d732bb1a3489ee2489ac7b5ec6d6108
Pratham Mohan <pratham.parker14@gmail.com>
2025-08-16 23:56:34 +0530

    Time: 0 ms (100.00%) | Memory: 56.51 MB (24.06%) - LeetSync

--- 852-Peak_index_in_mountain_array/Solution.java
+++ 852-Peak_index_in_mountain_array/Solution.java
@@ -5,11 +5,11 @@ class Solution {
 
         while (start < end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] < arr[mid + 1]) {
+            if (arr[mid] < arr[mid + 1])
                 start = mid + 1;
-            } else {
+             else
                 end = mid;
-            }
+
         }
         return start;
     }
