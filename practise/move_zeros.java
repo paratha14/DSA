e67ca969d6eeb07fc25826ecfee81dc9c95e935e
Pratham Mohan <pratham.parker14@gmail.com>
2025-09-01 23:34:40 +0530

    Time: 1 ms (99.64%) | Memory: 46.38 MB (38.66%) - LeetSync

---
+++ practise/move_zeros.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] moveZeroes(int[] nums) {
+        int insertPos = 0;
+
+        // Step 1: Move all non-zero elements forward
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+
+        // Step 2: Fill the rest with zeros
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+
+        return nums;
+    }
+}
