28ebeff5bd3af541488b55755c7d9e753b157516
Pratham Mohan <pratham.parker14@gmail.com>
2025-12-29 20:29:49 +0530

    pattern2

--- pattern.java
+++ pattern.java
@@ -1,12 +1,16 @@
 class pattern{
     public static void main(String[] args) {
         pattern1(4);
+
+        pattern2(5);
     }
-"""printing
+
+/*          printing
             *
             * *
             * * *
-            * * * * """
+            * * * * */
+
     public static void pattern1(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
@@ -16,4 +20,23 @@ class pattern{
         }
     }
 
+    /* printing
+
+     * * * * *
+     * * * * *
+     * * * * *
+     * * * * *
+     * * * * *
+
+     */
+
+    public static void pattern2(int n){
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+
+        }
+    }
 }
