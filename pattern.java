9b0347e9cc3dacd61558d8c0673f34228b7a14de
Pratham Mohan <pratham.parker14@gmail.com>
2025-12-29 20:45:51 +0530

    pattern 4

--- pattern.java
+++ pattern.java
@@ -5,6 +5,8 @@ class pattern{
         pattern2(5);
         System.out.println("\n Next Pattern: \n");
         pattern3(5);
+        System.out.println("\n Next Pattern: \n");
+        pattern4(5);
     }
 
 /*          printing
@@ -42,6 +44,17 @@ class pattern{
         }
     }
 
+
+    /*
+     printing:
+     * * * * *
+     * * * *
+     * * *
+     * *
+     *
+
+     */
+
     public static void pattern3(int n){
         for (int i = n; i >0 ; i--) {
             for (int j = i; j >0 ; j--) {
@@ -51,6 +64,25 @@ class pattern{
             System.out.println();
 
 
+        }
+    }
+
+    /*
+    printing:
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+
+     */
+
+    public static void pattern4(int n){
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+            }
+                System.out.println();
         }
     }
 }
