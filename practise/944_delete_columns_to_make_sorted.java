247fb44de3aaba9b065fa9cd756db1484b614c16
Pratham Mohan <pratham.parker14@gmail.com>
2026-01-03 18:33:38 +0530

    Time: 7 ms (74.79%) | Memory: 47.25 MB (34.06%) - LeetSync

---
+++ practise/944_delete_columns_to_make_sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int rows = strs.length;
+        int cols = strs[0].length();
+        int count = 0;
+
+        for (int col = 0; col < cols; col++) {
+            for (int row = 1; row < rows; row++) {
+                if (strs[row].charAt(col) < strs[row - 1].charAt(col)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
